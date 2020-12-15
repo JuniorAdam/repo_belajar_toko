901eab09143234979083af9e3c9a81a121dbd2f2
Junior Adam <junior_adam_28rpl@student.smktelkom-mlg.sch.id>
2020-12-15 13:25:33 +0700

    versi 2.3.2

---
+++ app/Http/Controllers/customersController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Customers;
+use Illuminate\Support\Facades\Validator;
+
+class CustomersController extends Controller
+{
+    public function destroy($id)
+    {
+        $hapus = Customers::where('id_customers', $id)->delete();
+        if ($hapus) {
+            return Response()->json(['status' => 1]);
+        } else {
+            return Response()->json(['status' => 0]);
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'nama_customers' => 'required',
+                'tanggal_lahir'     =>    'required',
+                'gender'         => 'required',
+                'alamat'         => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $ubah = Customers::where('id_customers', $id)->update(
+            [
+                'nama_customers' => $request->nama_customers,
+                'tanggal_lahir'     =>    $request->tanggal_lahir,
+                'gender'         => $request->gender,
+                'alamat'         => $request->alamat
+            ]
+        );
+
+        if ($ubah) {
+            return Response()->json(['status' => 1]);
+        } else {
+            return Response()->json(['status' => 0]);
+        }
+    }
+
+    public function show()
+    {
+        $data_customers = Customers::all();
+
+        return Response()->json($data_customers);
+    }
+
+    public function detail($id)
+    {
+        if (Customers::where('id_customers', $id)->exists()) {
+            $data_customers = Customers::all()
+                ->where('id_customers', '=', $id);
+
+            return Response()->json($data_customers);
+        } else {
+            return Response()->json(['message' => '!!Data Tidak Ditemukan!!']);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'nama_customers' => 'required',
+                'tanggal_lahir'     =>    'required',
+                'gender'         => 'required',
+                'alamat'         => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Response()->json($validator->errors());
+        }
+
+        $simpan = Customers::create([
+            'nama_customers' => $request->nama_customers,
+            'tanggal_lahir'     => $request->tanggal_lahir,
+            'gender'         => $request->gender,
+            'alamat'         => $request->alamat
+        ]);
+
+        if ($simpan) {
+            return Response()->json(['status' => 1]);
+        } else {
+            return Response()->json(['status' => 0]);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register', 'UserController@register');
+Route::post('/login', 'UserController@login');
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::group(['middleware' => ['api.superadmin']], function () {
+        Route::delete('/customers/{id_customers}', 'CustomersController@destroy');
+        Route::delete('/product/{id_product}', 'ProductController@destroy');
+        Route::delete('/orders/{id_orders}', 'OrdersController@destroy');
+    });
+
+    Route::group(['middleware' => ['api.admin']], function () {
+        Route::post('/customers', 'CustomersController@store');
+        Route::put('/customers/{id_customers}', 'CustomersController@update');
+
+        Route::post('/product', 'ProductController@store');
+        Route::put('/product/{id_product}', 'ProductController@update');
+
+        Route::post('/orders', 'OrdersController@store');
+        Route::put('/orders/{id_orders}', 'OrdersController@update');
+    });
+
+    Route::get('/customers', 'CustomersController@show');
+    Route::get('/customers/{id_customers}', 'CustomersController@detail');
+
+
+    Route::get('/product', 'ProductController@show');
+    Route::get('/product/{id_product}', 'ProductController@detail');
+
+
+    Route::get('/orders', 'OrdersController@show');
+    Route::get('/orders/{id_orders}', 'OrdersController@detail');
+});
